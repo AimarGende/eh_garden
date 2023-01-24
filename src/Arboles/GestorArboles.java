5fe536a5772f99c13e597e732cedce59868f3824
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-24 09:35:42 +0100

    PrepareStatementYabrol

--- src/Arboles/GestorArboles.java
+++ src/Arboles/GestorArboles.java
@@ -1,10 +1,11 @@
 package Arboles;
 import java.sql.Connection;
 import java.sql.DriverManager;
-
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GestorArboles {
@@ -20,7 +21,7 @@ public class GestorArboles {
 		final int VISUALIZAR_ARBOLES=4;
 		final int SALIR=0;
 		int opcion=0;
-		
+		Arbol arbol=new Arbol();
 		Scanner sc=new Scanner(System.in);
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");
@@ -38,15 +39,15 @@ public class GestorArboles {
 			opcion=Integer.parseInt(sc.nextLine());
 			switch(opcion) {
 				case INSERTAR_ARBOL :
-					insertarArbol(sc,st);
+					insertarArbol(sc,arbol);
 					System.out.println("se ha insertado el arbol correctamente");
 					break;
 				case ELIMINAR_ARBOL:
-					eliminarArbol(sc,st);
+					eliminarArbol(sc);
 					System.out.println("Se ha eliminado el arbol");
 					break;
 				case MODIFICAR_ARBOL:
-					modificarArbol(sc,st);
+					modificarArbol(sc);
 					System.out.println("Se ha modificado el arbol");
 					break;
 				case VISUALIZAR_ARBOLES:
@@ -63,35 +64,39 @@ public class GestorArboles {
 
 
 
-	private static void insertarArbol(Scanner sc, Statement st) throws SQLException {
-		String nombre;
-		String nombreCien;
-		String habitat;
-		double altura;
-		String origen;
+	private static void insertarArbol(Scanner sc,Arbol arbol) throws SQLException {
+		PreparedStatement pt=conexion.prepareStatement("INSERT INTO arboles (nombre_comun, nombre_cientifico, habitat, altura, origen) VALUES(?,?,?,?,?);");
+		
 		System.out.println("Introduce el nombre del arbol");
-		nombre=sc.nextLine();
+		arbol.setNombreComun(sc.nextLine());
 		System.out.println("Introduce el nombre cientifico del arbol");
-		nombreCien=sc.nextLine();
+		arbol.setNombreCientifico(sc.nextLine());
 		System.out.println("Introduce el habitat del arbol");
-		habitat=sc.nextLine();
+		arbol.setHabitat(sc.nextLine());
 		System.out.println("Introduce la altura del arbol");
-		altura=Double.parseDouble(sc.nextLine());
+		arbol.setAltura(Double.parseDouble(sc.nextLine()));
 		System.out.println("Introduce el origen del arbol");
-		origen=sc.nextLine();
-		String sentencia="INSERT INTO arboles (nombre_comun, nombre_cientifico, habitat, altura, origen) VALUES('"+nombre+"','"+nombreCien+"','"+habitat+"',"+altura+",'"+origen+"');";
-		st.execute(sentencia);
+		arbol.setOrigen(sc.nextLine());
+		
+		pt.setString(1, arbol.getNombreComun());
+		pt.setString(2, arbol.getNombreCientifico());
+		pt.setString(3, arbol.getHabitat());
+		pt.setDouble(4, arbol.getAltura());
+		pt.setString(5, arbol.getOrigen());
+		
+		pt.execute();
 	}
 	
-	private static void eliminarArbol(Scanner sc, Statement st) throws SQLException {
+	private static void eliminarArbol(Scanner sc) throws SQLException {
 		int id;
+		PreparedStatement pt=conexion.prepareStatement("DELETE FROM arboles WHERE id= ? ;");
 		System.out.println("Inserta el id que quieres eliminar");
 		id=Integer.parseInt(sc.nextLine());
-		String sentencia="DELETE FROM arboles WHERE id="+id+";";
-		st.execute(sentencia);
+		pt.setInt(1, id);
+		pt.execute();
 	}
 	
-	private static void modificarArbol(Scanner sc, Statement st) throws SQLException {
+	private static void modificarArbol(Scanner sc) throws SQLException {
 		String atrib;
 		String newValue;
 		int id;
@@ -102,22 +107,34 @@ public class GestorArboles {
 		atrib=sc.nextLine().toLowerCase();
 		System.out.println("Introduce el nuevo valor del atibuto:");
 		newValue=sc.nextLine();
-
+		
+		PreparedStatement prep=conexion.prepareStatement("UPDATE arboles SET "+atrib+"= (?) WHERE id="+id+";");
 		if(atrib.equals("altura")) {
-			st.execute("UPDATE arboles SET "+atrib+"= "+Double.parseDouble(newValue)+" WHERE id="+id+";");
+			prep.setDouble(1, Double.parseDouble(newValue));
 		}
 		else {
-			st.execute("UPDATE arboles SET "+atrib+"= '"+newValue+"' WHERE id="+id+";");
+			prep.setString(1, newValue);
 		}
-		
+		prep.executeUpdate();
 	}
 
 	private static void visualizarArboles(Statement st, Scanner sc) throws SQLException {
 		String select="SELECT * FROM arboles ";
 		ResultSet result=st.executeQuery(select);
+		ArrayList<Arbol> arbolList=new ArrayList<Arbol>();
 		while(result.next()) {
-			System.out.println(result.getInt(1)+"-"+result.getString(2)+"-"+result.getString(3)+"-"+result.getString(4)+"-"+result.getDouble(5)+"-"+result.getString(6));
+			Arbol arbol=new Arbol();
+			arbol.setId(result.getInt("id"));
+			arbol.setNombreComun(result.getString("nombre_comun"));
+			arbol.setNombreCientifico(result.getString("nombre_cientifico"));
+			arbol.setHabitat(result.getString("habitat"));
+			arbol.setAltura(result.getDouble("altura"));
+			arbol.setOrigen(result.getString("origen"));
+			
+			arbolList.add(arbol);
+			System.out.println(arbol);
 		}
+		
 	}
 
 
